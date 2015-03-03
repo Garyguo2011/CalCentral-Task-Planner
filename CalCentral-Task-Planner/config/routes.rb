2cb469119ceadab154e5f978a8e4f9c66689c82c
saasbook <saasbook@saasbook.(none)>
2015-03-02 21:17:08 -0800

    start set up auth system

--- CalCentral-Task-Planner/config/routes.rb
+++ CalCentral-Task-Planner/config/routes.rb
@@ -1,6 +1,6 @@
 CalCentralTaskPlanner::Application.routes.draw do
   resources :tasks
-
+  root to: 'tasks#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
