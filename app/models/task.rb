16b4f45df557005698409c81f680349c9a556ea5
Xinran Guo <xguo@berkeley.edu>
2015-03-06 22:51:32 -0800

    add task field kind, release date, course

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,3 @@
 class Task < ActiveRecord::Base
-  attr_accessible :due, :status, :title
+  attr_accessible :due, :status, :title, :course, :kind, :release
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+tasks =[
+{:title => 'Midterm1', :course => 'CS164', :kind => 'Exam', :release => '4/Mar/2015 23:59:00 -0800', :due => '4/Mar/2015 23:59:00 -0800', :status => 'New'},
+{:title => 'Homework5', :course => 'CS169', :kind => 'Homework', :release => '9/Mar/2015 23:59:00 -0800', :due => '16/Mar/2015 23:59:00 -0800', :status => 'New'},
+{:title => 'Project2', :course => 'CS164', :kind => 'Project', :release => '9/Mar/2015 23:59:00 -0800', :due => '31/Mar/2015 23:59:00 -0800', :status => 'Ongoing'},
+{:title => 'Vatamin#5', :course => 'CS186', :kind => 'Homework', :release => '27/Feb/2015 23:59:00 -0800', :due => '2/Mar/2015 23:59:00 -0800', :status => 'Complete'},
+{:title => 'Quiz#2', :course => 'CS169', :kind => 'Exam', :release => '12/Mar/2015 23:59:00 -0800', :due => '19/Mar/2015 23:59:00 -0800', :status => 'New'}]
+
+tasks.each do |task|
+	Task.create!(task)
+end
