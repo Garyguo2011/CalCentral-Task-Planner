4e3c81c228652b42d5b1b1446e494ae071a316fa
Xinran Guo <xguo@berkeley.edu>
2015-03-13 18:23:04 -0700

    fix some routes

--- spec/requests/subtasks_spec.rb
+++ spec/requests/subtasks_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe "Subtasks" do
+  before(:each) do
+    login_user
+  end
+
   describe "GET /subtasks" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get subtasks_path
+      get task_subtasks_path(:task_id => 1)
       response.status.should be(200)
     end
   end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe "Tasks" do
+  before(:each) do
+    login_user
+  end
+
   describe "GET /tasks" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
