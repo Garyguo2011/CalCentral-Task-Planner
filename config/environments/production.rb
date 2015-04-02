331f1cd0b7c1d2783bd09b5763b7f597fa87eda5
Jinge Zhang <jingezhang@berkeley.edu>
2015-04-02 16:26:14 -0700

    deploy on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ CalcentralTaskPlanner::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+  
 
   # Generate digests for assets URLs
   config.assets.digest = true
