1ed10922a94fcf28906b96cddbad1fc53d127297
Jinge Zhang <jingezhang@berkeley.edu>
2015-04-02 17:03:54 -0700

    add precompile to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,6 @@ CalcentralTaskPlanner::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.assets.paths << Rails.root.join('public', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
